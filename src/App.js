6b699eeb923b36f637c2f0447f2be2d99f70055f
Meysam Sefidkar <kwpasefidkar@gmail.com>
2023-06-27 04:05:05 +0330

    Add Spinner

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import {useState} from "react";
 
 const App = () => {
 
+    const [loading, setLoading] = useState(false);
     const [getContacts, setContacts] = useState([]);
 
     return (
         <div className="App">
             <Navbar/>
-            <Contacts contacts={getContacts}/>
+            <Contacts contacts={getContacts} loading={loading}/>
         </div>
     );
 }
--- src/components/contact/Contacts.jsx
+++ src/components/contact/Contacts.jsx
@@ -1,8 +1,9 @@
 import {CURRENTLINE, ORANGE, PINK} from "../../helpers/colors";
 import {Fragment} from "react";
 import Contact from "./Contact";
+import Spinner from "../Spinner";
 
-const Contacts = ({contacts}) => {
+const Contacts = ({contacts, loading}) => {
     return (
         <Fragment>
             <section className='container'>
@@ -19,22 +20,27 @@ const Contacts = ({contacts}) => {
                     </div>
                 </div>
             </section>
-            <section className='container'>
-                <div className='row'>
-                    {
-                        contacts.length > 0 ? contacts.map(c => (
-                            <Contact key={c.id} contact={c}/>
-                        )) : (
-                            <div className='text-center py-5' style={{backgroundColor: CURRENTLINE}}>
-                                <p className='h3' style={{color: ORANGE}}>
-                                    Kontakt nicht gefunden...
-                                </p>
-                                <img src={require('../../assets/no-found.gif')} alt='nicht gefunden' className='w-25'/>
-                            </div>
-                        )
-                    }
-                </div>
-            </section>
+            {
+                loading ? <Spinner/> : (
+                    <section className='container'>
+                        <div className='row'>
+                            {
+                                contacts.length > 0 ? contacts.map(c => (
+                                    <Contact key={c.id} contact={c}/>
+                                )) : (
+                                    <div className='text-center py-5' style={{backgroundColor: CURRENTLINE}}>
+                                        <p className='h3' style={{color: ORANGE}}>
+                                            Kontakt nicht gefunden...
+                                        </p>
+                                        <img src={require('../../assets/no-found.gif')} alt='nicht gefunden'
+                                             className='w-25'/>
+                                    </div>
+                                )
+                            }
+                        </div>
+                    </section>
+                )
+            }
         </Fragment>
     )
 }
