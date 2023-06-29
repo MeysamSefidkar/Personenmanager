dae5fd645350b5be3f2bbdb8d264825ce62a522c
Meysam Sefidkar <kwpasefidkar@gmail.com>
2023-06-30 02:14:25 +0330

    Create json-server and add contacts page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import './App.css';
 import {Routes, Route, Navigate} from "react-router-dom";
+import axios from "axios";
 
 import {AddContact, Contact, Contacts, EditContact, Navbar, ViewContact} from "./components";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 const App = () => {
 
     const [loading, setLoading] = useState(false);
     const [getContacts, setContacts] = useState([]);
+    const [getGroups, setGroups] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+
+                const {data: contactsData} = await axios.get("http://localhost:9000/contacts");
+                const {data: groupsData} = await axios.get("http://localhost:9000/groups");
+                setContacts(contactsData);
+                setGroups(groupsData);
+
+                setLoading(false);
+            } catch (err) {
+                console.log(err.message);
+                setLoading(false);
+            }
+        }
+
+        fetchData();
+    }, []);
+
 
     return (
         <div className="App">
--- src/components/contacts/Contact.jsx
+++ src/components/contacts/Contact.jsx
@@ -1,13 +1,13 @@
 import {CURRENTLINE, CYAN, ORANGE, PURPLE, RED} from "../../helpers/colors";
 
-const Contact = () => {
+const Contact = ({contact}) => {
     return (
         <div className='col-md-6'>
             <div style={{backgroundColor: CURRENTLINE}} className='card my-2'>
                 <div className='card-body'>
                     <div className='row align-items-center d-flex justify-content-around'>
                         <div className='col-md-4 col-sm-4'>
-                            <img src='https://via.placeholder.com/200' alt=""
+                            <img src={contact.photo} alt={contact.fullname}
                                  style={{border: `1px solid ${PURPLE}`}} className='img-fluid roundeds'/>
                         </div>
                         <div className='col-md-7 col-sm-7'>
@@ -15,19 +15,19 @@ const Contact = () => {
                                 <li className='list-group-item list-group-item-dark'>
                                     Vorname und Nachname : {" "}
                                     <span className='fw-bold'>
-                                                    Meysam Sefidkar
+                                                    {contact.fullname}
                                                 </span>
                                 </li>
                                 <li className='list-group-item list-group-item-dark'>
                                     Handynummer : {" "}
                                     <span className='fw-bold'>
-                                                    09163060179
+                                                    {contact.mobile}
                                                 </span>
                                 </li>
                                 <li className='list-group-item list-group-item-dark'>
                                     Email Adresse : {" "}
                                     <span className='fw-bold'>
-                                                    kwpasefidkar@gmail.com
+                                                    {contact.email}
                                                 </span>
                                 </li>
                             </ul>
